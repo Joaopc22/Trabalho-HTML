b7fa25cbcdf60e700ffa3f8ffb54ac0ee43c2a54
Joaopc22 <101648378+Joaopc22@users.noreply.github.com>
2022-06-21 12:07:25 -0300

    TV-tubo

---
+++ trabalho/main.js
@@ -0,0 +1,57 @@
+// gerencia o Canvas
+
+const canvas = document.querySelector('canvas');
+const ctx = canvas.getContext('2d');
+
+const width = canvas.width = 1040;
+
+const height = canvas.height = 528;
+
+// gera um número aleatório
+
+function random(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+// gera uma cor aleatória
+
+function randomRGB() {
+  return `rgb(192,192,192)`;
+}
+
+
+//define um vetor de bolas
+const bolas = [];
+
+while (bolas.length < 1000) {
+   const size = random(10,10);
+   const bola = new Bola(
+      // posição de sempre uma bola de distância
+      // fora das bordas para evitar erros de desenho
+      random(0 + size,width - size),
+      random(0 + size,height - size),
+      random(-7,7),
+      random(-7,7),
+      randomRGB(),
+      size
+   );
+
+   //atualiza o vetor
+  bolas.push(bola);
+}
+
+//realiza um loop em todas as bolas geradas
+function loop() {
+   ctx.fillStyle = 'rgba(0, 0, 0, 0.25)';
+   ctx.fillRect(0, 0,  width, height);
+
+   for (const bola of bolas) {
+    bola.draw();
+    bola.update();
+    bola.collisionDetect(bolas);
+   }
+
+   requestAnimationFrame(loop);
+}
+
+loop();
